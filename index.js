b6270ba8f490eebb9d651a173de575470b348dcb
Kristoffer Walker <kris@kixx.name>
2013-05-27 14:24:16 -0400

    creating and deleting a database
     On branch master
    
    	modified:   index.js
    	modified:   manage.sh
    	modified:   test.js

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ var HTTP = require('http')
 
 
 exports.request = function (opts) {
+  opts = opts || {};
+
   var d = IOU.newDefer()
     , reqOpts
     , req
 
   reqOpts = {
-    hostname: opts.hostname
-  , port: opts.port
-  , path: opts.path
+    hostname: opts.hostname || 'localhost'
+  , port: opts.port || 5984
+  , path: opts.path || ''
   , method: opts.method
+  , headers: {
+      'accept': 'application/json'
+    }
   };
+
+  if (opts.username) {
+    if (!opts.password) {
+      throw new Error("opts.password must be provided with opts.username");
+    }
+    var auth = exports.basicAuth(opts.username, opts.password);
+    reqOpts.headers['Authorization'] = auth;
+  }
+
+  if (!/^\//.test(reqOpts.path)) {
+    throw new Error("opts.path must start with a '/'.");
+  }
 
   req = HTTP.request(reqOpts, function (res) {
     var rv = Object.create(null)
@@ -27,16 +44,38 @@ exports.request = function (opts) {
     });
 
     res.on('end', function () {
-      rv.body = body;
-      return Object.freeze(rv);
+      rv.body = JSON.parse(body);
+      d.keep(Object.freeze(rv));
+      return;
     });
   });
 
   req.on('error', function (err) {
-    d.fail(err);
+    err = err || {};
+    var rv
+
+    if (err.code === 'EHOSTUNREACH') {
+      rv = new Error("CouchDB server could not be reached at '"+ reqOpts.hostname +"'.");
+      rv.code = err.code;
+    } else if (err.code === 'ENOTFOUND') {
+      rv = new Error("No CouchDB server found at '"+ reqOpts.hostname +"'.");
+      rv.code = err.code;
+    } else if (err.code === 'ECONNREFUSED') {
+      rv = new Error("The server at '"+ reqOpts.hostname +"' refused the connection.");
+      rv.code = err.code;
+    } else {
+      rv = err;
+    }
+
+    d.fail(rv);
   });
 
   req.end();
 
   return d.promise
+};
+
+exports.basicAuth = function (username, password) {
+  var str = username +':'+ password
+  return 'Basic '+ new Buffer(str).toString('base64');
 };
--- test.js
+++ test.js
@@ -2,27 +2,205 @@ var ASSERT = require('assert')
 
   , OLDLADY = require('./index')
 
+  , USERNAME
+  , PASSWORD
+  , HOSTNAME
+  , PORT
+
+!(function () {
+  var uri = process.argv[2] || '@'
+    , parts = uri.split('@')
+    , creds, host
+
+  if (parts.length > 1) {
+    creds = parts[0];
+    host = parts[1];
+  } else {
+    creds = ':';
+    host = parts[0];
+  }
+
+  parts = creds.split(':');
+  USERNAME = parts[0] || '';
+  PASSWORD = parts[1] || '';
+  parts = host.split(':');
+  HOSTNAME = parts[0] || 'localhost';
+  PORT = parts[1] || 5984;
+}());
+
+console.log('Testing with:');
+console.log("username:", USERNAME)
+console.log("password:", PASSWORD)
+console.log("hostname:", HOSTNAME)
+console.log("port:", PORT)
+
 var tests = [];
 
 
 tests.push(function (done) {
-    "";
+  "It should throw an error if the path does not begin with a slash.";
 
-    function onSuccess(res) {
-      console.log(res);
-      return done();
-    }
+  try {
+    OLDLADY.request({path: 'foo'})
+  } catch (err) {
+    equal(err.message, "opts.path must start with a '/'.", "no '/' error");
+  }
 
-    function onFailure(err) {
-      printError(err);
-      wrapAssertion(function () {
-        assert(false, ".failure() handler should not be called.")
-      });
-    };
+  return done();
+});
 
-    OLDLADY.request()
-      .then(onSuccess)
-      .failure(onFailure)
+tests.push(function (done) {
+  "It should report unreachable host.";
+
+  function onSuccess(res) {
+    printError(err);
+    wrapAssertion(function () {
+      assert(false, ".then() handler should not be called.")
+    });
+  }
+
+  function onFailure(err) {
+    wrapAssertion(function () {
+      equal(err.code, 'ENOTFOUND');
+      equal(err.message, "No CouchDB server found at 'foo.bar.baz'.")
+    });
+    return done();
+  };
+
+  var opts = defaultOpts({
+    hostname: 'foo.bar.baz'
+  });
+
+  OLDLADY.request(opts)
+    .then(onSuccess)
+    .failure(onFailure)
+});
+
+tests.push(function (done) {
+  "It should deny access without credentials.";
+
+  function onSuccess(res) {
+    data = res.body;
+    equal(res.statusCode, 401);
+    contentType = res.headers['content-type'];
+    assert(/^application\/json/.test(contentType), contentType);
+    equal(data.error, "unauthorized");
+    equal(data.reason, "You are not a server admin.");
+    return done();
+  }
+
+  function onFailure(err) {
+    printError(err);
+    wrapAssertion(function () {
+      assert(false, ".failure() handler should not be called.")
+    });
+  };
+
+  var opts = defaultOpts({
+    path: '/_config'
+  });
+
+  OLDLADY.request(opts)
+    .then(onSuccess)
+    .failure(onFailure)
+});
+
+tests.push(function (done) {
+  "It should respond with configurations.";
+
+  function onSuccess(res) {
+    equal(res.statusCode, 200);
+    contentType = res.headers['content-type'];
+    data = res.body;
+
+    assert(/^application\/json/.test(contentType), contentType);
+    assert(data.hasOwnProperty('stats'), 'has stats');
+    assert(data.hasOwnProperty('replicator'), 'has replicator');
+    assert(data.hasOwnProperty('log'), 'has log');
+    assert(data.hasOwnProperty('httpd'), 'has httpd');
+    assert(data.hasOwnProperty('couch_httpd_auth'), 'has couch_httpd_auth');
+    assert(data.hasOwnProperty('couchdb'), 'has couchdb');
+    return done();
+  }
+
+  function onFailure(err) {
+    printError(err);
+    wrapAssertion(function () {
+      assert(false, ".failure() handler should not be called.")
+    });
+  };
+
+  var opts = defaultOpts({
+    path: '/_config'
+  , username: USERNAME
+  , password: PASSWORD
+  });
+
+  OLDLADY.request(opts)
+    .then(onSuccess)
+    .failure(onFailure)
+});
+
+tests.push(function (done) {
+  "It should create a database.";
+
+  function onSuccess(res) {
+    equal(res.statusCode, 201);
+    contentType = res.headers['content-type'];
+    assert(/^application\/json/.test(contentType), contentType);
+    data = res.body;
+    assert(data.ok);
+    return done();
+  }
+
+  function onFailure(err) {
+    printError(err);
+    wrapAssertion(function () {
+      assert(false, ".failure() handler should not be called.")
+    });
+  };
+
+  var opts = defaultOpts({
+    method: 'PUT'
+  , path: '/oldlady_tests'
+  , username: USERNAME
+  , password: PASSWORD
+  });
+
+  OLDLADY.request(opts)
+    .then(onSuccess)
+    .failure(onFailure)
+});
+
+tests.push(function (done) {
+  "It should delete a database.";
+
+  function onSuccess(res) {
+    equal(res.statusCode, 200);
+    contentType = res.headers['content-type'];
+    assert(/^application\/json/.test(contentType), contentType);
+    data = res.body;
+    assert(data.ok);
+    return done();
+  }
+
+  function onFailure(err) {
+    printError(err);
+    wrapAssertion(function () {
+      assert(false, ".failure() handler should not be called.")
+    });
+  };
+
+  var opts = defaultOpts({
+    method: 'DELETE'
+  , path: '/oldlady_tests'
+  , username: USERNAME
+  , password: PASSWORD
+  });
+
+  OLDLADY.request(opts)
+    .then(onSuccess)
+    .failure(onFailure)
 });
 
 // End of testing.
@@ -33,6 +211,22 @@ tests.push(function () { console.log('PASSED'); });
 
 
 // Helpers
+
+function defaultOpts(opts) {
+  var rv = {
+    hostname: HOSTNAME
+  , port: PORT
+  , method: 'GET'
+  , path: '/'
+  };
+
+  rv = Object.keys(opts).reduce(function (rv, key) {
+    rv[key] = opts[key];
+    return rv;
+  }, rv);
+
+  return rv;
+}
 
 function type(obj) {
   return typeof obj;
