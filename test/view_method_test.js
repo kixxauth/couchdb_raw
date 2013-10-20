2122574fb9374a71d1dd5489b8cf31b971216eb6
Kristoffer Walker <kris@kixx.name>
2013-10-20 11:07:56 -0400

    Test PUT update a view
     On branch master
    
    	modified:   test/view_method_test.js

--- test/view_method_test.js
+++ test/view_method_test.js
@@ -11,12 +11,8 @@ exports["view doument operations"] = {
       , views, fixture
       , removeFixture
 
-    views = {
-      'by_model': {map: "function (doc) { emit(doc.class_name, doc); }"}
-    };
-
     fixture = this.fixture = {
-      views: {'my_app': views}
+      views: {'by_model': {map: "function (doc) { emit(doc.class_name, 1); }"}}
     };
 
     removeFixture = this.removeFixture = {};
@@ -26,8 +22,7 @@ exports["view doument operations"] = {
     }
 
     function setUpdatePath() {
-      var views = {'foo': {map: "function (doc) { emit(doc.id, doc); }"}}
-      return {path: '/'+ dbname +'/_design/'+ updateDoc, data: {views: views}};
+      return {path: '/'+ dbname +'/_design/'+ updateDoc, data: fixture};
     }
 
     function setDeletePath() {
@@ -149,4 +144,101 @@ exports["view doument operations"] = {
       .then(test.done)
       .failure(test.done);
   },
+
+  "PUT/update and GET": function (test) {
+    var dbname = this.dbname
+      , docname = this.updateDoc
+      , fixture = this.fixture
+
+    test.expect(17);
+
+    // Update the document:
+    function putDocument() {
+      var doc = fixture;
+      doc.views.by_model.reduce = "function (keys, values) { return sum(values); }";
+
+      var promise = COUCH.request({
+        method: 'PUT'
+      , path: '/'+ dbname +'/_design/'+ docname
+      , data: doc
+      , rev: fixture._rev
+      , hostname: HOSTNAME
+      , port: PORT
+      , username: USERNAME
+      , password: PASSWORD
+      });
+      return promise;
+    }
+
+    // Retrieve the document:
+    function getDoc(putResponse) {
+      var promise = COUCH.request({
+        method: 'GET'
+      , path: '/'+ dbname +'/_design/'+ docname
+      , hostname: HOSTNAME
+      , port: PORT
+      , username: USERNAME
+      , password: PASSWORD
+      }).then(function (res) {
+        return {put: putResponse, get: res};
+      });
+      return promise;
+    }
+
+    function getView(responses) {
+      var promise = COUCH.request({
+        method: 'GET'
+      , path: '/'+ dbname +'/_design/'+ docname + '/_view/by_model'
+      , hostname: HOSTNAME
+      , port: PORT
+      , username: USERNAME
+      , password: PASSWORD
+      }).then(function (res) {
+        responses.getView = res;
+        return responses;
+      });
+      return promise;
+    }
+
+    // Test the results:
+    function runTests(responses) {
+      var get = responses.get
+        , put = responses.put
+        , getView = responses.getView
+
+      // Test the PUT
+      test.strictEqual(put.statusCode, 201, 'PUT status code')
+      test.assertJSON(put, 'PUT JSON')
+      test.ok(put.body.ok, 'ok')
+      test.equal(put.body.id, '_design/'+ docname, 'doc.id')
+      test.equal(typeof put.body.rev, 'string', 'doc.rev')
+      test.equal(Object.keys(put.body).length, 3, 'PUT Object.keys(doc)')
+
+      // Test the GET
+      test.strictEqual(get.statusCode, 200, 'GET status code')
+      test.assertJSON(get, 'GET JSON')
+      test.equal(get.body._id, '_design/'+ docname, 'GET doc._id')
+      test.equal(typeof get.body._rev, 'string', 'GET doc._rev')
+      test.equal(put.body.rev, get.body._rev, 'correct revision')
+      test.equal(get.body.views.by_model.map, 'function (doc) { emit(doc.class_name, 1); }', 'doc.views map')
+      test.equal(get.body.views.by_model.reduce, 'function (keys, values) { return sum(values); }', 'doc.views reduce')
+
+      // Test GET view
+      test.strictEqual(getView.statusCode, 200, 'GET status code')
+      test.assertJSON(getView, 'GET JSON')
+      test.ok(Array.isArray(getView.body.rows), 'GET view rows')
+      test.ok(typeof getView.body.rows[0].value === 'number', 'GET view value')
+
+      // Make sure to return undefined.
+      return;
+    }
+
+    putDocument()
+      .then(getDoc)
+      .then(getView)
+      .then(runTests)
+      .then(test.done)
+      .failure(test.done);
+  },
+
 };
